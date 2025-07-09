092ea52a6dd5560473169b1b85a36bd46226da4c
Anurag-Deshmukh2505 <anurag.deshmukh25@gmail.com>
2025-07-09 19:03:58 +0530

    26. Remove Duplicates from sorted array

---
+++ Array/RemoveDupSortedArr.java
@@ -0,0 +1,16 @@
+package Array;
+
+public class RemoveDupSortedArr {
+
+    public static int removeDuplicates(int[] nums){
+        int j = 0;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] != nums[j]){
+                j++;
+                nums[j] = nums[i];
+            }
+        }
+
+        return j + 1;
+    }
+}
